736b747e691cf05043e7a33bf93632a777b2c4d6
tvseries188 <103586760+tvseries188@users.noreply.github.com>
2022-04-22 22:10:26 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ echo "
 
 Copyright (c) 2021 Itz-fork
 
---> Join @NexaBotsUpdates
---> Follow Itz-fork on Github
+--> Join @AnimeColony
+--> Join on @zippercartoonistrequestgroup 
 "
 python3 -m unzipper
